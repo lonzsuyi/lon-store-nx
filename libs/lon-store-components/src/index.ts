7baf732d0f040dc51d5f5d17864db8b4858400db
Lon Zhu <lonzsuyi@gmail.com>
2025-02-02 10:29:56 +1100

    feat:Add Dialog component

--- libs/lon-store-components/src/index.ts
+++ libs/lon-store-components/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/Icon/Icon';
 export * from './lib/Picture/Picture';
 export * from './lib/Rate/Rate';
+export * from './lib/Dialog/Dialog';
 export * from './lib/TextField/TextField';
 export * from './lib/Button/Button';
